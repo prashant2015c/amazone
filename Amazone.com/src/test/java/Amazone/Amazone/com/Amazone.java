427f7f0de69b3cc1c09775077400519fcd2278b8
prashant2015c <prashant2015c@gmail.com>
2018-06-19 23:31:02 +0530

    ok

--- Amazone.com/src/test/java/Amazone/Amazone/com/Amazone.java
+++ Amazone.com/src/test/java/Amazone/Amazone/com/Amazone.java
@@ -1,6 +1,14 @@
 package Amazone.Amazone.com;
 
+import javax.swing.text.Element;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import com.google.common.base.Verify;
 
 public class Amazone {
 	static ChromeDriver driver;
@@ -10,6 +18,15 @@ public class Amazone {
 		System.setProperty("webdriver.chrome.driver", "C:\\eclipse\\chromedriver_win32\\chromedriver.exe");
 		 driver = new ChromeDriver();
 		 driver.get("https://www.amazon.com/");
+		 String title = driver.getTitle();
+	WebElement el =  driver.findElement(By.xpath("//*[@id='nav-xshop']/a[3]"));
+	
+	Actions ac = new Actions(driver);
+	
+	ac.moveToElement(el).click().keyDown(el,Keys.SHIFT).sendKeys(el , "hello").keyUp(el,Keys.SHIFT);
+	
+		 Verify.verify(title.contains("amz"),"amzone");
+
 
 	}
 
